f17199e18e66116cd15825c33babf4669a3c8d3a
Emil Wall <emil.wall@consid.se>
2024-10-11 11:58:57 +0200

    Add settings management

---
+++ lib/data/sp_helper.dart
@@ -0,0 +1,30 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SPHelper {
+  static const keyName = 'name';
+  static const keyImage = 'image';
+  Future<bool> setSettings(String name, String image) async {
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await Future.wait([
+        prefs.setString(keyName, name),
+        prefs.setString(keyImage, image),
+      ]);
+      //throw Exception("test");
+      return true;
+    } on Exception catch (ex) {
+      print(ex.toString());
+      return false;
+    }
+  }
+
+  Future<Map<String, String>> getSettings() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String name = prefs.getString(keyName) ?? '';
+    final String image = prefs.getString(keyImage) ?? '';
+    return {
+      keyName: name,
+      keyImage: image,
+    };
+  }
+}
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mindful_app/data/sp_helper.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -8,13 +9,83 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  final TextEditingController txtName = TextEditingController();
+  final List<String> images = ['Lake', 'Mountain', 'Sea', 'Country'];
+  String selectedImage = 'Lake';
+
+  @override
+  void initState() {
+    super.initState();
+    getSettings().then((value) {
+      String message = value
+          ? 'The settings have been fetched'
+          : 'The settings were not fetched';
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(message),
+        duration: const Duration(seconds: 3),
+      ));
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: const Placeholder(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: txtName,
+              decoration: const InputDecoration(hintText: 'Enter your name'),
+            ),
+            DropdownButton(
+              value: selectedImage,
+              items: images
+                  .map((String value) => DropdownMenuItem(
+                        value: value,
+                        child: Text(value),
+                      ))
+                  .toList(),
+              onChanged: (newValue) {
+                setState(() {
+                  selectedImage = newValue!;
+                });
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          saveSettings().then((value) {
+            String message = value
+                ? 'The settings have been saved'
+                : 'The settings were not saved';
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(message),
+              duration: const Duration(seconds: 3),
+            ));
+          });
+        },
+        child: const Icon(Icons.save),
+      ),
     );
+  }
+
+  Future<bool> saveSettings() async {
+    final SPHelper helper = SPHelper();
+    return await helper.setSettings(txtName.text, selectedImage);
+  }
+
+  Future getSettings() async {
+    final SPHelper helper = SPHelper();
+    Map<String, String> settings = await helper.getSettings();
+    selectedImage = settings['image'] ?? 'Country';
+    txtName.text = settings['name'] ?? '';
+    setState(() {});
+    return true;
   }
 }
