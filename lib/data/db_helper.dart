0ef3a8bde2825f7bfea65d1383082fbcb10313a2
Emil Wall <emil.wall@consid.se>
2024-10-15 07:21:59 +0200

    Refactor db

--- lib/data/db_helper.dart
+++ lib/data/db_helper.dart
@@ -1,23 +1,37 @@
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
-import 'package:sembast/sembast_io.dart';
+import 'package:sembast_web/sembast_web.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'quote.dart';
 
 class DbHelper {
-  DatabaseFactory dbFactory = databaseFactoryIo;
+  DatabaseFactory dbFactory = databaseFactoryWeb;
   Database? database;
   final store = intMapStoreFactory.store('quotes');
+  static final DbHelper _instance = DbHelper._internal();
+  DbHelper._internal();
+  factory DbHelper() {
+    return _instance;
+  }
+
+  Future<Database> get _database async {
+    return (database ??= await _openDb());
+  }
 
   Future<Database> _openDb() async {
-    final docsDir = await getApplicationDocumentsDirectory();
-    final dbPath = join(docsDir.path, 'quotes.db');
+    var path = '/assets/db';
+    if (!kIsWeb) {
+      final docsDir = await getApplicationDocumentsDirectory();
+      path = docsDir.path;
+    }
+    final dbPath = join(path, 'quotes.db');
     final db = await dbFactory.openDatabase(dbPath);
     return db;
   }
 
   Future<int> insertQuote(Quote quote) async {
     try {
-      Database db = await _openDb();
+      Database db = await _database;
       int id = await store.add(db, quote.toMap());
       return id;
     } on Exception catch (_) {
@@ -26,7 +40,7 @@ class DbHelper {
   }
 
   Future<List<Quote>> getQuotes() async {
-    Database db = await _openDb();
+    Database db = await _database;
     final finder = Finder(sortOrders: [SortOrder('a')]);
     final snapshot = await store.find(db, finder: finder);
     final quotes = snapshot
@@ -37,7 +51,7 @@ class DbHelper {
 
   Future<int?> removeQuote(Quote quote) async {
     try {
-      Database db = await _openDb();
+      Database db = await _database;
       Finder finder = Finder(filter: Filter.equals('q', quote.text));
       int removed = await store.delete(db, finder: finder);
       return removed;
